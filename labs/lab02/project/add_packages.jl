2614f95ac88e2b00c0838f3ab42127b722b73cae
glebq <glebb2005@mail.ru>
2026-03-07 23:31:26 +0300

    lab02 done

---
+++ labs/lab02/project/add_packages.jl
@@ -0,0 +1,21 @@
+##!/usr/bin/env julia
+## add_packages.jl
+using Pkg
+Pkg.activate(".")
+packages = [
+    "DrWatson",
+    "DifferentialEquations",
+    "Plots",
+    "DataFrames",
+    "CSV",
+    "JLD2",
+    "Literate",
+    "IJulia",
+    "BenchmarkTools",
+    "Quarto",
+    "LaTeXStrings"
+]
+println("Установка базовых пакетов...")
+Pkg.add(packages)
+println("\n✅ Все пакеты установлены!")
+println("Для проверки: using DrWatson, DifferentialEquations, Plots")
